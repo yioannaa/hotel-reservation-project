e1ac7a7d0acc6cae149697f2be47d4c61cd575c9
Joanna Krzyżanowska <joanna.krzyzanowska@o2.pl>
2019-07-14 10:27:45 +0200

    init

---
+++ src/test/java/jk/hotelreservationproject/HotelReservationProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package jk.hotelreservationproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelReservationProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
