8d072c485da27415112afe012ab9258330e9dec9
Pandragona <asiuspospolitus@gmail.com>
2019-07-21 14:15:43 +0200

    contact - form, if logged in , autoinserting user email, shows errors if any elements from form are invalid, automailing - confirmation that mail was sent

--- src/main/java/jk/hotelreservationproject/controller/ContactController.java
+++ src/main/java/jk/hotelreservationproject/controller/ContactController.java
@@ -1,22 +1,30 @@
 package jk.hotelreservationproject.controller;
 
 import jk.hotelreservationproject.model.Contact;
+import jk.hotelreservationproject.service.AutoMailingService;
 import jk.hotelreservationproject.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class ContactController {
 
     ContactService contactService;
+    AutoMailingService autoMailingService;
 
     @Autowired
-    public ContactController(ContactService contactService) {
+    public ContactController(ContactService contactService, AutoMailingService autoMailingService) {
         this.contactService = contactService;
+        this.autoMailingService = autoMailingService;
     }
 
     @GetMapping("/contact")
@@ -25,8 +33,22 @@ public class ContactController {
         if(auth != null){
             UserDetails userDetails = (UserDetails) auth.getPrincipal();
             contact.setEmail(userDetails.getUsername());
+            model.addAttribute("loggedEmail", userDetails.getUsername());
         }
         model.addAttribute("contact", contact);
         return "contact";
     }
+
+    @PostMapping("/contact")
+    public String contact(@ModelAttribute @Valid Contact contact, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return "contact";
+        }
+        contactService.addContact(contact);
+        autoMailingService.sendMessage(contact.getEmail(),
+                "Hotel Message Confirmation",
+                "Thank you for your message :)");
+        return "redirect:/";
+    }
+
 }
---
+++ src/main/java/jk/hotelreservationproject/service/AutoMailingService.java
@@ -0,0 +1,24 @@
+package jk.hotelreservationproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AutoMailingService {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendMessage(String to, String subject, String message){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
